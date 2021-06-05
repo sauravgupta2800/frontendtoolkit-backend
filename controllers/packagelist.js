79b7ae40fee2e71e2030b7f9f1968dd7ff5a12af
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-06-05 13:59:25 +0530

    bloglist controller created

--- controllers/packagelist.js
+++ controllers/packagelist.js
@@ -1,7 +1,19 @@
+const axios = require("axios");
+const config = require("../utils/config");
 const packagelistRouter = require("express").Router();
 
 packagelistRouter.get("/", async (request, response) => {
-  response.send({ name: "Saurav" });
+  const { PACKAGE_LIST_API } = config;
+  const { q } = request.query;
+  if (!q) response.status(400).send({ message: "Search key is required" });
+  axios
+    .get(`${PACKAGE_LIST_API}`, { params: request.query })
+    .then(({ data }) => {
+      response.send({ data: data, count: (data || []).length });
+    })
+    .catch(() => {
+      response.status(400).send({ message: "Error While querying" });
+    });
 });
 
 module.exports = packagelistRouter;
--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,11 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+const PACKAGE_LIST_API = "https://api.npms.io/v2/search/suggestions";
+
 const PORT = process.env.PORT;
 
 module.exports = {
   PORT,
+  PACKAGE_LIST_API,
 };
