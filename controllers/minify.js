d5273f29938e90531f63f5b735a0315bbaf6d240
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-06-20 11:47:59 +0530

    minify css added

--- controllers/minify.js
+++ controllers/minify.js
@@ -1,16 +1,32 @@
 // https://github.com/terser/terser
 const { minify } = require("terser");
-
+const CleanCSS = require("clean-css");
 const minifyRouter = require("express").Router();
 
 minifyRouter.post("/", async (request, response) => {
-  if (!request.body.code)
-    response.status(400).send({ message: "code is required" });
-  try {
-    const result = await minify(request.body.code, { sourceMap: true });
-    response.send({ data: result.code });
-  } catch {
-    response.status(400).send({ message: "Error while  minification" });
+  const { code, type = "javascript" } = request.body;
+  if (!code) response.status(400).send({ message: "code is required" });
+  if (type === "javascript") {
+    try {
+      const result = await minify(code, { sourceMap: true });
+      response.send({ data: result.code });
+    } catch {
+      response.status(400).send({ message: "Error while js/ts  minification" });
+    }
+  } else if (type === "css") {
+    try {
+      const minified = new CleanCSS({ inline: "none" }).minify(code);
+      minified.css = minified.styles
+        ? minified.styles
+        : minified.errors
+            .concat(minified.warnings)
+            .map((msg) => `/* ${msg} */`)
+            .join("\n");
+
+      response.send({ data: minified.css });
+    } catch {
+      response.status(400).send({ message: "Error while css minification" });
+    }
   }
 });
 
